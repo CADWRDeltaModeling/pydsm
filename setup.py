3606bab55027a586545511ff52d5866b7c712777
Nicky Sandhu <psandhu@water.ca.gov>
2020-03-26 22:30:14 -0700

    adding subpackages to setuptools packaging

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     include_package_data=True,
     keywords='pydsm',
     name='pydsm',
-    packages=find_packages(include=['pydsm']),
+    packages=find_packages(include=['pydsm','pydsm.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
