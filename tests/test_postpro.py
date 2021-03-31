27b7d7202ba3dff7f1232269db8badc03e7b1cc2
Nicky Sandhu <psandhu@water.ca.gov>
2021-03-30 17:37:53 -0700

    adding test for timewindow for postpro

--- tests/test_postpro.py
+++ tests/test_postpro.py
@@ -3,6 +3,7 @@ import pydsm
 from pydsm import postpro
 import pytest
 import numpy as np
+import pandas as pd
 
 def test_postprocessor():
     study = postpro.Study('observed', 'data/sample_obs_small.dss')
@@ -13,5 +14,7 @@ def test_postprocessor():
     assert p.df is not None
     p.store_processed()
     p2=postpro.PostProcessor(study,loc,vtype)
-    p2.load_processed()
+    p2.load_processed(timewindow='03OCT2016 0000 - 05OCT2016 0000')
+    assert p2.df.index[0] == pd.to_datetime('03OCT2016 0000')
+    assert p2.df.index[-1] == pd.to_datetime('05OCT2016 0000')
     #assert_frame_equal(p.df, p2.df,check_names=False, check_column_type=False, check_like=False)
