dee1581e45db9f6a48c652cdb7f68ab876cfb4d9
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 01:19:26 +0800

    Delete tests directory

--- tests/test_model_h5_type.py
+++
@@ -1,8 +0,0 @@
-import h5py
-from pydsm import dsm2h5
-def test_model_type():
-    """ Test for hydro or qual identification based on reading the groups of the .h5 file"""
-    with h5py.File('data/historical_v82.h5','r') as h5f:
-        assert dsm2h5.get_model(h5f) == 'hydro'
-    with h5py.File('data/historical_v82_ec.h5','r') as h5f:
-        assert dsm2h5.get_model(h5f) == 'qual'
--- tests/test_pydsmcli.py
+++
@@ -1,6 +0,0 @@
-import pydsm
-
-def test_pydsm_cli():
-    ''' test the pydsm command line interface works correctly'''
-    pass
-    
