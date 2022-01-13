b54352c3052362be08405d2917410c4b6236b452
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 01:18:01 +0800

    Delete docsrc directory

--- docsrc/doall.bat
+++
@@ -1,7 +0,0 @@
-rem only needed if you add submodules etc..
-call conda activate dev_pydsm
-call sphinx-apidoc -f -o . ..\pydsm
-call make clean
-call make html
-rem call xcopy /y /s /e _build\* ..\docs
-call xcopy /y /s /e _build\* ..\..\pydsm-gh
