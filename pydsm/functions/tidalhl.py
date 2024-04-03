3844628a914d904c9ab7b897ff89284905a1d3e4
Nicky Sandhu <psandhu@water.ca.gov>
2024-04-03 10:16:10 -0700

    reformatted with black

--- pydsm/functions/tidalhl.py
+++ pydsm/functions/tidalhl.py
@@ -4,7 +4,9 @@ import numba
 from vtools.functions.filter import cosine_lanczos
 
 
-def get_smoothed_resampled(df, cutoff_period='2h', resample_period='1min', interpolate_method='pchip'):
+def get_smoothed_resampled(
+    df, cutoff_period="2h", resample_period="1min", interpolate_method="pchip"
+):
     """Resample the dataframe (indexed by time) to the regular period of resample_period using the interpolate method
 
     Furthermore the cosine lanczos filter is used with a cutoff_period to smooth the signal to remove high frequency noise
@@ -31,10 +33,9 @@ def get_smoothed_resampled(df, cutoff_period='2h', resample_period='1min', inter
 
 @numba.jit(nopython=True)
 def lmax(arr):
-    '''Local maximum: Returns value only when centered on maximum
-    '''
+    """Local maximum: Returns value only when centered on maximum"""
     idx = np.argmax(arr)
-    if idx == len(arr)/2:
+    if idx == len(arr) / 2:
         return arr[idx]
     else:
         return np.NaN
@@ -42,10 +43,9 @@ def lmax(arr):
 
 @numba.jit(nopython=True)
 def lmin(arr):
-    '''Local minimum: Returns value only when centered on minimum
-    '''
+    """Local minimum: Returns value only when centered on minimum"""
     idx = np.argmin(arr)
-    if idx == len(arr)/2:
+    if idx == len(arr) / 2:
         return arr[idx]
     else:
         return np.NaN
@@ -65,10 +65,13 @@ def periods_per_window(moving_window_size: str, period_str: str) -> int:
         int: number of periods in the moving window rounded to an integer
     """
 
-    return int(pd.Timedelta(moving_window_size)/pd.to_timedelta(pd.tseries.frequencies.to_offset(period_str)))
+    return int(
+        pd.Timedelta(moving_window_size)
+        / pd.to_timedelta(pd.tseries.frequencies.to_offset(period_str))
+    )
 
 
-def tidal_highs(df, moving_window_size='7h'):
+def tidal_highs(df, moving_window_size="7h"):
     """Tidal highs (could be upto two highs in a 25 hr period)
 
     Args:
@@ -84,13 +87,13 @@ def tidal_highs(df, moving_window_size='7h'):
     period_str = df.index.freqstr
     periods = periods_per_window(moving_window_size, period_str)
     dfmax = df.rolling(moving_window_size, min_periods=periods).apply(lmax, raw=True)
-    dfmax = dfmax.shift(periods=-(periods//2-1))
+    dfmax = dfmax.shift(periods=-(periods // 2 - 1))
     dfmax = dfmax.dropna()
-    dfmax.columns = ['max']
+    dfmax.columns = ["max"]
     return dfmax
 
 
-def tidal_lows(df, moving_window_size='7h'):
+def tidal_lows(df, moving_window_size="7h"):
     """Tidal lows (could be upto two lows in a 25 hr period)
 
     Args:
@@ -106,13 +109,19 @@ def tidal_lows(df, moving_window_size='7h'):
     period_str = df.index.freqstr
     periods = periods_per_window(moving_window_size, period_str)
     dfmin = df.rolling(moving_window_size, min_periods=periods).apply(lmin, raw=True)
-    dfmin = dfmin.shift(periods=-(periods//2-1))
+    dfmin = dfmin.shift(periods=-(periods // 2 - 1))
     dfmin = dfmin.dropna()
-    dfmin.columns = ['min']
+    dfmin.columns = ["min"]
     return dfmin
 
 
-def get_tidal_hl(df, cutoff_period='2h', resample_period='1min', interpolate_method='pchip', moving_window_size='7h'):
+def get_tidal_hl(
+    df,
+    cutoff_period="2h",
+    resample_period="1min",
+    interpolate_method="pchip",
+    moving_window_size="7h",
+):
     """Get Tidal highs and lows
 
     Args:
@@ -129,7 +138,7 @@ def get_tidal_hl(df, cutoff_period='2h', resample_period='1min', interpolate_met
 
     Returns:
 
-        tuple of DataFrame: Tidal high and tidal low time series 
+        tuple of DataFrame: Tidal high and tidal low time series
     """
     dfs = get_smoothed_resampled(df, cutoff_period, resample_period, interpolate_method)
     return tidal_highs(dfs), tidal_lows(dfs)
@@ -152,22 +161,24 @@ def get_tidal_amplitude(dfh, dfl):
         DataFrame: Amplitude timeseries, at the times of the low following the high being used for amplitude calculation
     """
     dfamp = pd.concat([dfh, dfl], axis=1)
-    dfamp = dfamp[['min']].dropna().join(dfamp[['max']].ffill())
-    return pd.DataFrame(dfamp['max']-dfamp['min'], columns=['amplitude'])
+    dfamp = dfamp[["min"]].dropna().join(dfamp[["max"]].ffill())
+    return pd.DataFrame(dfamp["max"] - dfamp["min"], columns=["amplitude"])
 
 
 def get_value_diff(df, percent_diff=False):
-    '''
+    """
     Get the difference of values of each element in the dataframe
     The times in the dataframe may or may not coincide as this is a slice of irregularly sampled time series
     On any error, the returned value is np.nan
-    '''
+    """
     try:
         arr = [df[c].dropna() for c in df.columns]
         if percent_diff:
-            value_diff = 100.0 * (arr[0].values[0]-arr[1].values[0])/arr[1].values[0]
+            value_diff = (
+                100.0 * (arr[0].values[0] - arr[1].values[0]) / arr[1].values[0]
+            )
         else:
-            value_diff = arr[0].values[0]-arr[1].values[0]
+            value_diff = arr[0].values[0] - arr[1].values[0]
         return value_diff
     except:
         return np.nan
@@ -188,24 +199,27 @@ def get_tidal_amplitude_diff(dfamp1, dfamp2, percent_diff=False):
 
         DataFrame: Difference dfamp1-dfamp2 or % Difference (dfamp1-dfamp2)/dfamp2*100  for values within +/- 4H of each other
     """
-    dfamp = pd.concat([dfamp1, dfamp2], axis=1).dropna(how='all')
-    dfamp.columns = ['2', '1']
-    tdelta = '4h'
-    sliceamp = [slice(t-pd.to_timedelta(tdelta), t+pd.to_timedelta(tdelta)) for t in dfamp.index]
+    dfamp = pd.concat([dfamp1, dfamp2], axis=1).dropna(how="all")
+    dfamp.columns = ["2", "1"]
+    tdelta = "4h"
+    sliceamp = [
+        slice(t - pd.to_timedelta(tdelta), t + pd.to_timedelta(tdelta))
+        for t in dfamp.index
+    ]
     ampdiff = [get_value_diff(dfamp[sl], percent_diff) for sl in sliceamp]
     return pd.DataFrame(ampdiff, index=dfamp.index)
 
 
 def get_index_diff(df):
-    '''
+    """
     Get the difference of index values of each element in the dataframe
     The times in the dataframe may or may not coincide
-    The difference is in Timedelta and is converted to minutes 
+    The difference is in Timedelta and is converted to minutes
     On any error, the returned value is np.nan
-    '''
+    """
     try:
         arr = [df[c].dropna() for c in df.columns]
-        tidal_phase_diff = (arr[0].index[0]-arr[1].index[0]).total_seconds()/60.
+        tidal_phase_diff = (arr[0].index[0] - arr[1].index[0]).total_seconds() / 60.0
         return tidal_phase_diff
     except:
         return np.nan
@@ -214,7 +228,7 @@ def get_index_diff(df):
 def get_tidal_phase_diff(dfh2, dfl2, dfh1, dfl1):
     """Calculates the phase difference between df2 and df1 tidal highs and lows
 
-    Scans +/- 4 hours in df1 to get the highs and lows in that windows for df2 to 
+    Scans +/- 4 hours in df1 to get the highs and lows in that windows for df2 to
     get the tidal highs and lows at the times of df1
 
 
@@ -232,28 +246,40 @@ def get_tidal_phase_diff(dfh2, dfl2, dfh1, dfl1):
 
         DataFrame: Phase difference (dfh2-dfh1) and (dfl2-dfl1) in minutes
     """
-    '''
-    '''
-    tdelta = '4h'
-    sliceh1 = [slice(t-pd.to_timedelta(tdelta), t+pd.to_timedelta(tdelta)) for t in dfh1.index]
-    slicel1 = [slice(t-pd.to_timedelta(tdelta), t+pd.to_timedelta(tdelta)) for t in dfl1.index]
+    """
+    """
+    tdelta = "4h"
+    sliceh1 = [
+        slice(t - pd.to_timedelta(tdelta), t + pd.to_timedelta(tdelta))
+        for t in dfh1.index
+    ]
+    slicel1 = [
+        slice(t - pd.to_timedelta(tdelta), t + pd.to_timedelta(tdelta))
+        for t in dfl1.index
+    ]
     dfh21 = pd.concat([dfh2, dfh1], axis=1)
-    dfh21.columns = ['2', '1']
+    dfh21.columns = ["2", "1"]
     dfl21 = pd.concat([dfl2, dfl1], axis=1)
-    dfl21.columns = ['2', '1']
+    dfl21.columns = ["2", "1"]
     high_phase_diff, low_phase_diff = [get_index_diff(dfh21[sl]) for sl in sliceh1], [
-        get_index_diff(dfl21[sl]) for sl in slicel1]
-    merged_diff = pd.merge(pd.DataFrame(high_phase_diff, index=dfh1.index), pd.DataFrame(
-        low_phase_diff, index=dfl1.index), how='outer', left_index=True, right_index=True)
+        get_index_diff(dfl21[sl]) for sl in slicel1
+    ]
+    merged_diff = pd.merge(
+        pd.DataFrame(high_phase_diff, index=dfh1.index),
+        pd.DataFrame(low_phase_diff, index=dfl1.index),
+        how="outer",
+        left_index=True,
+        right_index=True,
+    )
     return merged_diff.iloc[:, 0].fillna(merged_diff.iloc[:, 1])
 
 
-def get_tidal_hl_zerocrossing(df, round_to='1min'):
-    '''
-    Finds the tidal high and low times using zero crossings of the first derivative. 
+def get_tidal_hl_zerocrossing(df, round_to="1min"):
+    """
+    Finds the tidal high and low times using zero crossings of the first derivative.
 
     This works for all situations but is not robust in the face of noise and perturbations in the signal
-    '''
+    """
     zc, zi = zerocross(df)
     if round_to:
         zc = pd.to_datetime(zc).round(round_to)
@@ -261,39 +287,39 @@ def get_tidal_hl_zerocrossing(df, round_to='1min'):
 
 
 def zerocross(df):
-    '''
+    """
     Calculates the gradient of the time series and identifies locations where gradient changes sign
     Returns the time rounded to nearest minute where the zero crossing happens (based on linear derivative assumption)
-    '''
+    """
     diffdfv = pd.Series(np.gradient(df[df.columns[0]].values), index=df.index)
     indi = np.where((np.diff(np.sign(diffdfv))) & (diffdfv[1:] != 0))[0]
     # Find the zero crossing by linear interpolation
     zdb = diffdfv[indi].index
-    zda = diffdfv[indi+1].index
+    zda = diffdfv[indi + 1].index
     x = diffdfv.index
     y = diffdfv.values
-    dx = x[indi+1] - x[indi]
-    dy = y[indi+1] - y[indi]
-    zc = -y[indi] * (dx/dy) + x[indi]
+    dx = x[indi + 1] - x[indi]
+    dy = y[indi + 1] - y[indi]
+    zc = -y[indi] * (dx / dy) + x[indi]
     return zc, indi
+
 
 ##---- FUNCTIONS CACHED BELOW THIS LINE PERHAPS TO USE LATER? ---#
 
 
 def where_changed(df):
-    '''
-    '''
+    """ """
     diff = np.diff(df[df.columns[0]].values)
     wdiff = np.where(diff != 0)[0]
     wdiff = np.insert(wdiff, 0, 0)  # insert the first value i.e. zero index
-    return df.iloc[wdiff+1, :]
+    return df.iloc[wdiff + 1, :]
 
 
 def where_same(dfg, df):
-    '''
+    """
     return dfg only where its value is the same as df for the same time stamps
     i.e. the interesection locations with df
-    '''
+    """
     dfall = pd.concat([dfg, df], axis=1)
     return dfall[dfall.iloc[:, 0] == dfall.iloc[:, 1]].iloc[:, 0]
 
@@ -303,8 +329,8 @@ def limit_to_indices(df, si, ei):
 
 
 def filter_where_na(df, dfb):
-    '''
+    """
     remove values in df where dfb has na values
-    '''
+    """
     dfx = dfb.loc[df.index]
     return df.loc[dfx.dropna().index, :]
--- tests/test_tidalhl.py
+++ tests/test_tidalhl.py
@@ -9,27 +9,43 @@ from pydsm.functions import tidalhl
 
 @pytest.fixture
 def tidal_signal_with_gaps():
-    '''
+    """
     A simple tidal signal fixture
-    '''
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data',
-                                  'tidal_signal_with_gaps.csv'), parse_dates=[0], index_col=0, dtype='float')
-    dfh = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data',
-                                   'tidal_signal_with_gaps_highs.csv'), parse_dates=[0], index_col=0, dtype='float')
-    dfl = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data',
-                                   'tidal_signal_with_gaps_lows.csv'), parse_dates=[0], index_col=0, dtype='float')
+    """
+    df = pd.read_csv(
+        os.path.join(os.path.dirname(__file__), "data", "tidal_signal_with_gaps.csv"),
+        parse_dates=[0],
+        index_col=0,
+        dtype="float",
+    )
+    dfh = pd.read_csv(
+        os.path.join(
+            os.path.dirname(__file__), "data", "tidal_signal_with_gaps_highs.csv"
+        ),
+        parse_dates=[0],
+        index_col=0,
+        dtype="float",
+    )
+    dfl = pd.read_csv(
+        os.path.join(
+            os.path.dirname(__file__), "data", "tidal_signal_with_gaps_lows.csv"
+        ),
+        parse_dates=[0],
+        index_col=0,
+        dtype="float",
+    )
     return df, dfh, dfl
 
 
 @pytest.fixture
 def tidal_signal_with_gaps_doubled_shifted(tidal_signal_with_gaps):
-    '''
+    """
     A values doubled and shifted signal (25 minutes) based on tidal_signal_with_gaps
-    '''
+    """
     df, dfh, dfl = tidal_signal_with_gaps
-    df2 = 2*df  # increase amplitude to twice
+    df2 = 2 * df  # increase amplitude to twice
     # resample @ min resolution and shift by 25 mins
-    df2s = df2.resample('T').interpolate().shift(25, freq='T')
+    df2s = df2.resample("T").interpolate().shift(25, freq="T")
     # resample back at original resolution
     df2s = df2s.resample(tidal_signal_with_gaps.index.freq).interpolate()
     return df2s
@@ -37,23 +53,44 @@ def tidal_signal_with_gaps_doubled_shifted(tidal_signal_with_gaps):
 
 @pytest.fixture
 def tidal_signal_with_disturbances():
-    '''
+    """
     tidal signal with disturbances
-    '''
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'tidal_signal_with_disturbances.csv'),
-                     parse_dates=[0], index_col=0, dtype='float')
-    dfh = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'tidal_signal_with_disturbances_highs.csv'),
-                      parse_dates=[0], index_col=0, dtype='float')
-    dfl = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'tidal_signal_with_disturbances_lows.csv'),
-                      parse_dates=[0], index_col=0, dtype='float')
+    """
+    df = pd.read_csv(
+        os.path.join(
+            os.path.dirname(__file__), "data", "tidal_signal_with_disturbances.csv"
+        ),
+        parse_dates=[0],
+        index_col=0,
+        dtype="float",
+    )
+    dfh = pd.read_csv(
+        os.path.join(
+            os.path.dirname(__file__),
+            "data",
+            "tidal_signal_with_disturbances_highs.csv",
+        ),
+        parse_dates=[0],
+        index_col=0,
+        dtype="float",
+    )
+    dfl = pd.read_csv(
+        os.path.join(
+            os.path.dirname(__file__), "data", "tidal_signal_with_disturbances_lows.csv"
+        ),
+        parse_dates=[0],
+        index_col=0,
+        dtype="float",
+    )
     return df, dfh, dfl
 
 
 def _plot_highs_lows(dfh, dfl, df):
     import matplotlib.pyplot as plt
-    plt.plot(dfh, 'g^', label='highs')
-    plt.plot(dfl, 'r+', label='lows')
-    plt.plot(df, label='data')
+
+    plt.plot(dfh, "g^", label="highs")
+    plt.plot(dfl, "r+", label="lows")
+    plt.plot(df, label="data")
     plt.show()
 
 
@@ -80,7 +117,7 @@ def test_signal_with_disturbances(tidal_signal_with_disturbances):
 def test_amplitude_with_gaps(tidal_signal_with_gaps):
     df, dfh, dfl = tidal_signal_with_gaps
     dfamp = tidalhl.get_tidal_amplitude(dfh, dfl)
-    dfh2, dfl2 = 2*dfh, 2*dfl
+    dfh2, dfl2 = 2 * dfh, 2 * dfl
     dfamp2 = tidalhl.get_tidal_amplitude(dfh2, dfl2)
     dfdiff = tidalhl.get_tidal_amplitude_diff(dfamp, dfamp2)
-    assert_array_almost_equal(2*dfamp.values, dfamp2.values)
+    assert_array_almost_equal(2 * dfamp.values, dfamp2.values)
