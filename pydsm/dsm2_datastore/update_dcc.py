5d3cc5b8172a502fa4f6d67806d3edaaaca29a0d
Ycai <Yujia.Cai@water.ca.gov>
2026-08-13 10:53:01 -0700

    Add script for updating DCC data in gate.dss

--- pydsm/dsm2_datastore/update_dcc.py
+++ pydsm/dsm2_datastore/update_dcc.py
@@ -2,11 +2,11 @@ import os
 from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
+from dms_datastore.read_multi import read_ts_repo
 
 import click
 import pyhecdss
 import yaml
-
 
 
 import logging
@@ -15,11 +15,16 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logging.basicConfig(level=logging.WARNING, format="%(levelname)s: %(message)s")
 
 
-def update_ccf_dss(sdate, edate, dss_path, process_flow_file, dss_outfile, column_name):
-    process_data = pd.read_csv(process_flow_file, comment='#', index_col=0, parse_dates=True).loc[sdate:edate]
+def update_dcc_dss(sdate, edate, dss_path, processed_file, dss_outfile, column_name):
+    process_data = pd.read_csv(processed_file, comment='#', index_col=0, parse_dates=True).loc[sdate:edate]
     process_data = process_data[[column_name]]
+    process_data['dsm2_val'] = np.where(
+        process_data[column_name].astype(str).str.strip().str.lower() == 'closed', 0, 2
+    )
+    process_data = process_data[['dsm2_val']]
+
     if process_data.empty:
-       raise ValueError(f"No flow data found in {process_flow_file} for the period {sdate} to {edate}")
+       raise ValueError(f"No data found for the period {sdate} to {edate} in file {processed_file}")
     for i in pyhecdss.get_ts(dss_outfile,f'{dss_path}'):
         original_dss_data = i.data
         units = i.units
@@ -85,10 +90,11 @@ def main(config_file, site):
     """
     with open(config_file, "r") as f:
         config = yaml.safe_load(f)
-    ccf_config_name = ['ccf_gate_ele', 'ccf_ndup']
-    for s in ccf_config_name:
+
+    dcc_config_name = ['dcc']
+    for s in dcc_config_name:
         site_cfg = config[s]
-        update_ccf_dss(site_cfg['start'], site_cfg['end'], site_cfg['dss_path'],
+        update_dcc_dss(site_cfg['start'], site_cfg['end'], site_cfg['dss_path'],
                        site_cfg['processed_file'], site_cfg['outfile'], site_cfg['column_name'])
         logging.info(f"Updated DSM2 Gate DSS for site {s}")
 
